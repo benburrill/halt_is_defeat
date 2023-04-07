680b62b665de60d658bf1e3aac19aa11a094ba07
Ben Burrill <bburrill98@gmail.com>
2023-04-06 18:25:42 -0700

    Update tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -20,22 +20,26 @@ class Anywhere:
         return NotImplemented
 
 def test_declaration():
-    assert ps('int x = 42').parse(syn_vdecl()) == Declaration(
+    assert ps('int x = 42').expect(syn_vdecl()) == Declaration(
         Variable(const=False, type=DataType(TypeToken.INT), name='x'),
         IntLiteral(42, Anywhere()), Anywhere()
     )
 
-    assert ps("const byte x = 'B'").parse(syn_vdecl()) == Declaration(
+    assert ps("const byte x = 'B'").expect(syn_vdecl()) == Declaration(
         Variable(const=True, type=DataType(TypeToken.BYTE), name='x'),
         IntLiteral(ord('B'), Anywhere()), Anywhere()
     )
 
-    assert ps('bool x[5]').parse(syn_vdecl()) == Declaration(
+    assert ps('bool x[5]').expect(syn_vdecl()) == Declaration(
         Variable(const=False, type=ArrayType(TypeToken.BOOL), name='x'),
         ArrayInitializer(ArrayType(TypeToken.BOOL), IntLiteral(5, Anywhere())),
         Anywhere()
     )
 
+    with raises(ParserError): ps('int x').expect(syn_vdecl())
+    with raises(ParserError): ps('const x = 5').expect(syn_vdecl())
+    with raises(ParserError): ps('aint x = 5').expect(syn_vdecl())
+    with raises(ParserError): ps('int @x = 5').expect(syn_vdecl())
     with raises(ParserError): ps('int x = int x = 5').expect(syn_vdecl())
     with raises(ParserError): ps('const int x[5]').expect(syn_vdecl())
     with raises(ParserError): ps('int[] x[5]').expect(syn_vdecl())
