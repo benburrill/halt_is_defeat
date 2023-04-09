9a2d76e41d1724da6846175cd4bc281f9a18f202
Ben Burrill <bburrill98@gmail.com>
2023-04-09 15:43:18 -0700

    Fix parser entry point

--- hidc/parser/__main__.py
+++ hidc/parser/__main__.py
@@ -1,10 +1,10 @@
 from hidc.lexer import SourceCode
-from hidc.parser import parse, ps_program
+from hidc.parser import parse
 from pprint import pprint
 
 def main(filename):
     # not actually very "pretty", but what can you do.
-    pprint(parse(ps_program(), SourceCode.from_file(filename)))
+    pprint(parse(SourceCode.from_file(filename)))
 
 if __name__ == '__main__':
     import sys
